2923f01b9bd1e6d2d00a4c2c2fc7e5af728fa621
Ali Qureshi <parorrey@yahoo.com>
2023-06-01 20:13:17 +0500

    DB added

--- wp-content/themes/pi-more/header.php
+++ wp-content/themes/pi-more/header.php
@@ -54,7 +54,7 @@
 						wp_nav_menu(
 							array(
 								'container_id' => 'primary-menu',
-								'container_class' => ' mr-4      mt-4 p-4  lg:mt-0 lg:p-0 lg:bg-transparent lg:block',
+								'container_class' => ' mr-4 mt-4 p-4  lg:mt-0 lg:p-0 lg:bg-transparent lg:block',
 								'menu_class' => 'lg:flex lg:-mx-4 ',
 								'theme_location' => 'primary',
 								'li_class' => 'pb-5 pr-3 mr-4  pl-1 mr-1 text-left',
